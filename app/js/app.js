e5243f3a973c11fac12b8f564ec6f42d1d809ce3
Stas <stas@absoluteweb.com>
2024-03-03 21:16:19 +0200

    theme

---
+++ app/js/app.js
@@ -0,0 +1,18 @@
+//Import Plugins
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/swiper.scss';
+
+//FancyBox
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/src/Fancybox/Fancybox.scss";
+
+
+// Import CSS files
+import '../css/app.scss'
+
+// Import JS files
+import Foundation from 'foundation-sites' ;
+
+jQuery(document).foundation();
